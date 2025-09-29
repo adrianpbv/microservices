b6f5a34a32c44587690e7f7b27c423fc85e448b1
adrianj <adrianpbv@gmail.com>
2025-09-28 23:41:49 -0400

    section 10 Resilience patterns

---
+++ section_10/account/src/main/java/com/bankdemo/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,16 @@
+package com.bankdemo.accounts.service.client;
+
+import com.bankdemo.accounts.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// TODO add the fallback class
+@FeignClient(name = "loans", fallback = LoansFallback.class) // loans is the name of the instance on the Eureka server
+public interface LoansFeignClient {
+    @GetMapping(value = "api/fetch", consumes = "application/json")
+    ResponseEntity<LoansDto> fetchLoanDetails(@RequestHeader("eazybank-correlation-id") String correlationId,
+                                              @RequestParam String mobileNumber);
+}
